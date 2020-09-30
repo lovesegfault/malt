1df5f105801b41361a3f688827840a477c1ad990
Bernardo Meurer <bernardo@meurer.org>
2020-09-30 00:50:48 -0700

    main: clean

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-const PROGRAM: &[u8] = &[
-    // mov eax, 42 (0x2a)
-    0xb8, 0x2a, 0x00, 0x00, 0x00, //
-    // ret
-    0xc3,
-];
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut mmap = memmap::MmapOptions::new().len(PROGRAM.len()).map_anon()?;
-    mmap.copy_from_slice(PROGRAM);
-    let mmap = mmap.make_exec()?;
-    let function: fn() -> u8 = unsafe { std::mem::transmute(mmap.as_ptr()) };
-    let ret: u8 = function();
-    assert_eq!(ret, 42);
+    println!("Hello");
     Ok(())
 }
